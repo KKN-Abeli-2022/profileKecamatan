edaa0b5f15d9c96965a89b43b0d72bcc0afb47d4
subhanaldiansyah <subhanaldiansyah3@gmail.com>
2022-08-09 16:05:08 +0800

    mengubah path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.{ejs,js,html}'],
+  content: ['./public/**/*.{ejs,js,html}', './views/**/*.{ejs,js,html}'],
   darkMode: 'class',
   theme: {
     extend: {
