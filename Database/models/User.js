197a092271c0863204a64f3b9333f9693bfa6e25
maliefrr <aliefm144@gmail.com>
2022-08-11 09:52:08 +0800

    add user model

---
+++ Database/models/User.js
@@ -0,0 +1,29 @@
+const {Model} = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+    class User extends Model {
+        static associate(models) {
+            User.hasMany(models.Post, {foreignKey: "userId"});
+        }
+    }
+    User.init({
+        Username: {
+            type: DataTypes.STRING,
+            primaryKey: true
+        },
+        Email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        Password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },{
+        sequelize,
+        modelName: "profile_abeli",
+        tableName: "user",
+        timestamps: false
+    });
+    return User;
+}
