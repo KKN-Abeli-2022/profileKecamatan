a30fb7cdd40d9d70c119de74b82da95322434108
maliefrr <aliefm144@gmail.com>
2022-08-11 10:00:07 +0800

    fix text-shadow and delete static in user model

--- Database/models/User.js
+++ Database/models/User.js
@@ -1,9 +1,6 @@
 const {Model} = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
     class User extends Model {
-        static associate(models) {
-            User.hasMany(models.Post, {foreignKey: "userId"});
-        }
     }
     User.init({
         Username: {
